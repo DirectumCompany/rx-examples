0926f64dec3dd8162a5c703197ca6b69d9de9ddd
Шкляев Андрей (Shklyaev_AA) <Shklyaev_AA@directum.ru>
2019-01-17 17:06:22 +0400

    #77423 Добавлено перекрытие Docflow, цвет отметки изменён на фиолетовый

---
+++ src/Packages/Sungero.Examples/Sungero.Examples.Server/Sungero.Docflow/ModuleServerFunctions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+
+namespace Sungero.Examples.Module.Docflow.Server
+{
+  partial class ModuleFunctions
+  {
+    /// <summary>
+    /// Получить отметку о подписании документа.
+    /// </summary>
+    /// <returns>Строка в формате html.</returns>
+    public override string GetSignatureMark(Sungero.Docflow.IOfficialDocument document, int versionId)
+    {
+      string html = Resources.HtmlStampTemplateCustom;
+      
+      var signature = Sungero.Docflow.PublicFunctions.OfficialDocument.GetSignatureForStamp(document, versionId);
+      if (signature == null)
+        throw new Exception(Sungero.Docflow.OfficialDocuments.Resources.LastVersionNotApproved);
+      
+      #warning Заглушка, убрать в процессе реализации отметки для ПЭП.
+      if (signature.SignCertificate == null)
+        throw new Exception("Отсутствует сертификат");
+      
+      var signatureInfo = this.ParseSignatureSubject(signature);      
+      var signatoryName = string.Format("{0} {1}", signatureInfo.Surname, signatureInfo.GivenName).Trim();
+      if (string.IsNullOrEmpty(signatoryName))
+        signatoryName = signatureInfo.CounterpartyName;
+      
+      html = html.Replace("{SignatoryFullName}", signatoryName);
+      html = html.Replace("{Thumbprint}", signature.SignCertificate.Thumbprint.ToLower());
+      html = html.Replace("{Validity}", string.Format("{0} {1} {2} {3}",
+                                                      Company.Resources.From,
+                                                      signature.SignCertificate.NotBefore.Value.ToShortDateString(),
+                                                      Company.Resources.To,
+                                                      signature.SignCertificate.NotAfter.Value.ToShortDateString())
+                         );
+      
+      return html;
+    }
+  }
+}
---
+++ src/Packages/Sungero.Examples/Sungero.Examples.Shared/Sungero.Docflow/ModuleConstants.cs
@@ -0,0 +1,10 @@
+using System;
+using Sungero.Core;
+
+namespace Sungero.Examples.Module.Docflow.Constants
+{
+  public static class Module
+  {
+
+  }
+}
