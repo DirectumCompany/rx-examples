27318e1366e879df346865d8b5e7eed5c821619c
Сусеков Денис (Susekov_DY) <susekov_dy@directum.ru>
2019-01-21 14:01:35 +0400

    #77428 Штамп в перекрытии вх. счетов на оплату теперь почти такой же как в общем перекрытии. Поправлены комменты и ресурсы.

--- src/Packages/Sungero.Examples/Sungero.Examples.Server/IncomingInvoice/IncomingInvoiceServerFunctions.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.Server/IncomingInvoice/IncomingInvoiceServerFunctions.cs
@@ -10,12 +10,40 @@ namespace Sungero.Examples.Server
   partial class IncomingInvoiceFunctions
   {
     /// <summary>
-    /// Получить отметку "Принят к оплате".
+    /// Получить отметку об ЭП.
     /// </summary>
-    /// <returns>Строка в формате html.</returns>
+    /// <returns>Изображение отметки об ЭП в виде html.</returns>
+    /// <remarks>
+    /// В отметку об ЭП добавлены логотип, дата и время подписания, цвет отметки изменён на красный, текст изменен на "Принят к оплате".
+    /// </remarks>
     public override string GetSignatureMarkAsHtml(int versionId)
     {
-      return Examples.IncomingInvoices.Resources.HtmlStampTemplate;
+      string html = Examples.IncomingInvoices.Resources.HtmlStampTemplateCustom;
+      
+      var signature = Sungero.Docflow.PublicFunctions.OfficialDocument.GetSignatureForMark(_obj, versionId);
+      if (signature == null)
+        throw new Exception(Sungero.Docflow.OfficialDocuments.Resources.LastVersionNotApproved);
+      
+      #warning Заглушка, убрать в процессе реализации отметки для ПЭП.
+      if (signature.SignCertificate == null)
+        throw new Exception("Отсутствует сертификат");
+      var certificateSubject = Docflow.PublicFunctions.Module.GetCertificateSubject(signature);
+      var signatoryName = string.Format("{0} {1}", certificateSubject.Surname, certificateSubject.GivenName).Trim();
+      if (string.IsNullOrEmpty(signatoryName))
+        signatoryName = certificateSubject.CounterpartyName;
+      
+      html = html.Replace("{SignatoryFullName}", signatoryName);
+      html = html.Replace("{Thumbprint}", signature.SignCertificate.Thumbprint.ToLower());
+      html = html.Replace("{Validity}", string.Format("{0} {1} {2} {3}",
+                                                      Company.Resources.From,
+                                                      signature.SignCertificate.NotBefore.Value.ToShortDateString(),
+                                                      Company.Resources.To,
+                                                      signature.SignCertificate.NotAfter.Value.ToShortDateString())
+                         );
+      html = html.Replace("{SignDate}", signature.SigningDate.ToString("d"));
+      html = html.Replace("{SignTime}", signature.SigningDate.ToString("t"));
+      
+      return html;
     }
   }
 }
