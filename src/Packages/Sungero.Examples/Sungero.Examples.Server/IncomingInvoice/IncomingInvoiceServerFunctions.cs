e2128c2965d4d0819d95906a5b3a4d2c712db606
Сусеков Денис (Susekov_DY) <susekov_dy@directum.ru>
2019-01-28 12:49:44 +0400

    #77944 Пример перекрытия входящих счетов. Адаптировано под простую ЭП, изменен внешний вид отметки (пока разъезжается непонятно из-за чего)

--- src/Packages/Sungero.Examples/Sungero.Examples.Server/IncomingInvoice/IncomingInvoiceServerFunctions.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.Server/IncomingInvoice/IncomingInvoiceServerFunctions.cs
@@ -12,37 +12,87 @@ namespace Sungero.Examples.Server
     /// <summary>
     /// Получить отметку об ЭП.
     /// </summary>
+    /// <param name="document">Документ для преобразования.</param>
+    /// <param name="versionId">Id версии, для генерации.</param>
     /// <returns>Изображение отметки об ЭП в виде html.</returns>
-    /// <description>
-    /// Пример перекрытия логики наложения отметки об ЭП для входящего счета.
-    /// В отметку об ЭП добавлены логотип, дата и время подписания, цвет отметки изменён на красный, текст изменен на "ПРИНЯТ К ОПЛАТЕ".
-    /// </description>
     public override string GetSignatureMarkAsHtml(int versionId)
     {
-      string html = Examples.IncomingInvoices.Resources.HtmlStampTemplateCustom;
-      
-      var signature = Sungero.Docflow.PublicFunctions.OfficialDocument.GetSignatureForMark(_obj, versionId);
+      var signature = this.GetSignatureForMark(versionId);
       if (signature == null)
         throw new Exception(Sungero.Docflow.OfficialDocuments.Resources.LastVersionNotApproved);
       
-      #warning Заглушка, убрать в процессе реализации отметки для ПЭП.
-      if (signature.SignCertificate == null)
-        throw new Exception("Отсутствует сертификат");
-      var certificateSubject = Docflow.PublicFunctions.Module.GetCertificateSubject(signature);
-      var signatoryName = string.Format("{0} {1}", certificateSubject.Surname, certificateSubject.GivenName).Trim();
-      if (string.IsNullOrEmpty(signatoryName))
-        signatoryName = certificateSubject.CounterpartyName;
+      // В случае квалифицированной ЭП информацию для отметки брать из атрибутов субъекта сертификата.
+      if (signature.SignCertificate != null)
+        return this.GetSignatureMarkForCertificateAsHtml(signature);
       
-      html = html.Replace("{SignatoryFullName}", signatoryName);
-      html = html.Replace("{Thumbprint}", signature.SignCertificate.Thumbprint.ToLower());
-      html = html.Replace("{Validity}", string.Format("{0} {1} {2} {3}",
-                                                      Company.Resources.From,
-                                                      signature.SignCertificate.NotBefore.Value.ToShortDateString(),
-                                                      Company.Resources.To,
-                                                      signature.SignCertificate.NotAfter.Value.ToShortDateString())
-                         );
+      // В случае простой ЭП информацию для отметки брать из атрибутов подписи.
+      return this.GetSignatureMarkForSignatureAsHtml(signature);
+    }
+    
+    /// <summary>
+    /// Получить отметку об ЭП для подписи.
+    /// </summary>
+    /// <param name="signature">Подпись.</param>
+    /// <returns>Изображение отметки об ЭП для подписи в виде html.</returns>
+    /// <description>
+    /// Пример перекрытия логики наложения отметки о простой ЭП для входящего счета.
+    /// В отметке о простой ЭП изменены логотип и пропорции заголовка.
+    /// Также в отметку добавлены дата и время подписания.
+    /// Цвет отметки изменен на красный.
+    /// Текст отметки изменен на "ПРИНЯТ К ОПЛАТЕ".
+    /// </description>
+    public virtual string GetSignatureMarkForSignatureAsHtml(Sungero.Domain.Shared.ISignature signature)
+    {
+      if (signature == null)
+        return string.Empty;
+      
+      var signatoryFullName = signature.SignatoryFullName;
+      var signatoryId = signature.Signatory.Id;
+      
+      string html = Examples.IncomingInvoices.Resources.HtmlStampTemplateForSignatureCustom;
+      html = html.Replace("{SignatoryFullName}", signatoryFullName);
+      html = html.Replace("{SignatoryId}", signatoryId.ToString());
       html = html.Replace("{SigningDate}", signature.SigningDate.ToString("g"));
+      return html;
+    }
+    
+    /// <summary>
+    /// Получить отметку об ЭП для сертификата из подписи.
+    /// </summary>
+    /// <param name="signature">Подпись.</param>
+    /// <returns>Изображение отметки об ЭП для сертификата в виде html.</returns>
+    /// <description>
+    /// Пример перекрытия логики наложения отметки о квалифицированной ЭП для входящего счета.
+    /// В отметке о квалифицированной ЭП изменены логотип и пропорции заголовка.
+    /// Также в отметку добавлены дата и время подписания.
+    /// Цвет отметки изменен на красный.
+    /// Текст отметки изменен на "ПРИНЯТ К ОПЛАТЕ".
+    /// </description>
+    public virtual string GetSignatureMarkForCertificateAsHtml(Sungero.Domain.Shared.ISignature signature)
+    {
+      if (signature == null)
+        return string.Empty;
       
+      var certificate = signature.SignCertificate;
+      if (certificate == null)
+        return string.Empty;
+      
+      var certificateSubject = Docflow.PublicFunctions.Module.GetCertificateSubject(signature);
+      
+      var signatoryFullName = string.Format("{0} {1}", certificateSubject.Surname, certificateSubject.GivenName).Trim();
+      if (string.IsNullOrEmpty(signatoryFullName))
+        signatoryFullName = certificateSubject.CounterpartyName;
+      
+      string html = Examples.IncomingInvoices.Resources.HtmlStampTemplateForCertificateCustom;
+      html = html.Replace("{SignatoryFullName}", signatoryFullName);
+      html = html.Replace("{Thumbprint}", certificate.Thumbprint.ToLower());
+      var validity = string.Format("{0} {1} {2} {3}",
+                                   Company.Resources.From,
+                                   certificate.NotBefore.Value.ToShortDateString(),
+                                   Company.Resources.To,
+                                   certificate.NotAfter.Value.ToShortDateString());
+      html = html.Replace("{Validity}", validity);
+      html = html.Replace("{SigningDate}", signature.SigningDate.ToString("g"));
       return html;
     }
   }
