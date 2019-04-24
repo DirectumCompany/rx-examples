2defa2fa0a89ab27a84eed3c5e0c4f0e49654447
Котегов Валентин (Kotegov_VS) <Kotegov_VS@directum.ru>
2019-04-24 09:38:28 +0400

    #81916 Добавил получении имени комплекта из DCS. Имя комплекта пишется в ведущий документ, остальные документы заносятся как приложения. [Rx]

--- src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/ModuleClientFunctions.cs
+++ src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/ModuleClientFunctions.cs
@@ -17,7 +17,7 @@ namespace Sungero.Capture.Client
     /// <param name="deviceInfo">Путь к xml файлу DCS c информацией об устройствах ввода.</param>
     /// <param name="filesInfo">Путь к xml файлу DCS c информацией об импортируемых файлах.</param>
     /// <param name="folder">Путь к папке хранения файлов, переданных в пакете.</param>
-    public static void ProcessCapturedPackage(string senderLine, string instanceInfos, string deviceInfo, string filesInfo, string folder, 
+    public static void ProcessCapturedPackage(string senderLine, string instanceInfos, string deviceInfo, string filesInfo, string folder,
                                               string responsibleId, string firstPageClassifierName, string typeClassifierName)
     {
       // Найти ответственного.
@@ -29,6 +29,7 @@ namespace Sungero.Capture.Client
       }
       
       var filePath = GetScannedPackagePath(filesInfo, folder);
+      var sourceFileName = GetScannedPackageName(instanceInfos);
       if (string.IsNullOrEmpty(filePath))
       {
         Logger.Error(Resources.FileNotFoundFormat(filePath));
@@ -36,11 +37,11 @@ namespace Sungero.Capture.Client
       }
       
       // Разделить пакет на документы.
-      var arioUrl = Functions.Module.Remote.GetArioUrl();      
+      var arioUrl = Functions.Module.Remote.GetArioUrl();
       var documentGuids = SplitPackage(filePath, arioUrl, firstPageClassifierName);
       
       // Обработать пакет.
-      Functions.Module.Remote.ProcessSplitedPackage(documentGuids, int.Parse(responsibleId));           
+      Functions.Module.Remote.ProcessSplitedPackage(sourceFileName, documentGuids, int.Parse(responsibleId));
     }
     
     /// <summary>
@@ -66,15 +67,18 @@ namespace Sungero.Capture.Client
     /// <returns>Путь к пакету документов со сканера.</returns>
     public static string GetScannedPackagePath(string filesInfo, string folder)
     {
-       if (!File.Exists(filesInfo))
-       {
+      if (!File.Exists(filesInfo))
+      {
         Logger.Error(Resources.NoFilesInfoInPackage);
         return string.Empty;
       }
       
-      var filesXDoc = System.Xml.Linq.XDocument.Load(filesInfo);           
-      // При захвате из файловой системы (со сканера) создать документ на основе единственного файла в папке.
-      var fileElement = filesXDoc.Element("InputFilesSection").Element("Files").Elements().FirstOrDefault();
+      var filesXDoc = System.Xml.Linq.XDocument.Load(filesInfo);
+      var fileElement = filesXDoc
+        .Element("InputFilesSection")
+        .Element("Files")
+        .Elements()
+        .FirstOrDefault();
       if (fileElement == null)
       {
         Logger.Error(Resources.NoFilesInfoInPackage);
@@ -90,6 +94,34 @@ namespace Sungero.Capture.Client
       
       return filePath;
     }
-                   
+    
+    /// <summary>
+    /// Получить путь к пакету документов со сканера.
+    /// </summary>
+    /// <param name="instanceInfos">Путь к xml файлу DCS c информацией об экземплярах захвата и о захваченных файлах.</param>
+    /// <returns>Путь к пакету документов со сканера.</returns>
+    public static string GetScannedPackageName(string instanceInfos)
+    {
+      if (!File.Exists(instanceInfos))
+      {
+        Logger.Error(Resources.NoFilesInfoInPackage);
+        return string.Empty;
+      }
+
+      var filesXDoc = System.Xml.Linq.XDocument.Load(instanceInfos);
+      var fileElement = filesXDoc
+        .Element("CaptureInstanceInfoList")
+        .Element("FileSystemCaptureInstanceInfo")
+        .Element("Files")
+        .Elements()
+        .FirstOrDefault();
+      if (fileElement == null)
+      {
+        Logger.Error(Resources.NoFilesInfoInPackage);
+        return string.Empty;
+      }
+      
+      return fileElement.Element("FileDescription").Value;
+    }
   }
 }
--- src/Packages/Sungero.Capture/Sungero.Capture.Server/ModuleServerFunctions.cs
+++ src/Packages/Sungero.Capture/Sungero.Capture.Server/ModuleServerFunctions.cs
@@ -14,16 +14,18 @@ namespace Sungero.Capture.Server
     /// <summary>
     /// Создать документы в Rx и отправить задачу на проверку.
     /// </summary>
+    /// <param name="sourceFileName">Имя исходного файла, полученного с DCS.</param>
     /// <param name="documentGuids">Список Гуидов документов, на которые Арио разделил пакет.</param>
     /// <param name="responsibleId">Ид сотрудника, ответственного за проверку документов.</param>
     [Remote, Public]
-    public static void ProcessSplitedPackage(List<string> documentGuids, int responsibleId)
+    public static void ProcessSplitedPackage(string sourceFileName, List<string> documentGuids, int responsibleId)
     {
       var documents = new List<IOfficialDocument>();
-      var leadingDocument = CreateDocumentByGuid(documentGuids.First(), null);
+      var leadingDocument = CreateDocumentByGuid(sourceFileName, 0, documentGuids.First(), null);
       documentGuids = documentGuids.Skip(1).ToList();
+      int addendumNumber = 1;
       foreach (var documentGuid in documentGuids)
-        documents.Add(CreateDocumentByGuid(documentGuid, leadingDocument));
+        documents.Add(CreateDocumentByGuid(string.Empty, addendumNumber++, documentGuid, leadingDocument));
       
       if (leadingDocument != null)
         SendToResponsible(leadingDocument, documents, responsibleId);
@@ -42,23 +44,26 @@ namespace Sungero.Capture.Server
       var arioUrl = string.Empty;
       if (!(commandExecutionResult is DBNull) && commandExecutionResult != null)
         arioUrl = commandExecutionResult.ToString();
+      
       return arioUrl;
     }
     
     /// <summary>
     /// Создать документ в Rx, тело документа загружается из Арио.
     /// </summary>
+    /// <param name="name">Имя документа.</param>
+    /// <param name="addendumNumber">Номер приложения.</param>
     /// <param name="documentGuid">Гуид тела документа.</param>
     /// <param name="firstDoc">Ведущий документ.</param>
     /// <returns></returns>
-    public static Docflow.IOfficialDocument CreateDocumentByGuid(string documentGuid, IOfficialDocument leadingDoc)
+    public static Docflow.IOfficialDocument CreateDocumentByGuid(string name, int addendumNumber,string documentGuid, IOfficialDocument leadingDoc)
     {
       var arioUrl = Functions.Module.GetArioUrl();
       var arioConnector = new ArioExtensions.ArioConnector(arioUrl);
       var documentBody = arioConnector.GetDocumentByGuid(documentGuid);
       
       var document = SimpleDocuments.Create();
-      document.Name = Resources.DocumentNameFormat(document.Id);
+      document.Name = string.IsNullOrWhiteSpace(name) ? Resources.DocumentNameFormat(addendumNumber) : name;
       document.CreateVersionFrom(documentBody, "pdf");
       if (leadingDoc != null)
         document.Relations.AddFrom(Constants.Module.SimpleRelationRelationName, leadingDoc);
