f831fb6f236ac233936d97528ad2a6de2d44346e
Колзин Артем (Kolzin_AA) <Kolzin_AA@directum.ru>
2019-04-30 14:43:58 +0400

    #81996 #82192 Получение заголовка и псевдообязательность у входящего письма.

---
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/IncomingLetter/IncomingLetterSharedFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.SmartCapture.IncomingLetter;
+
+namespace Sungero.SmartCapture.Shared
+{
+  partial class IncomingLetterFunctions
+  {
+    public override void SetRequiredProperties()
+    {
+      base.SetRequiredProperties();
+      
+      // Содержание обязательно, только если это указано в метаданных.
+      _obj.State.Properties.Subject.IsRequired = _obj.Info.Properties.Subject.IsRequired;
+    }
+  }
+}
