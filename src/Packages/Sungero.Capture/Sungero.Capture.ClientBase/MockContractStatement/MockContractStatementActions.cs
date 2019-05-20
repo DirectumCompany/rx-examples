009dfc5aab9e1fb062026b1605f28167f02ad73a
Дмитриев Иван (Dmitriev_IA) <Dmitriev_IA@directum.ru>
2019-05-20 14:31:48 +0400

    #82716. Заготовка для MockContractStatement. [Rx]

---
+++ src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/MockContractStatement/MockContractStatementActions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Capture.MockContractStatement;
+
+namespace Sungero.Capture.Client
+{
+}
