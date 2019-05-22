988d30944eb4dde0b5aaa8ceee978eef6aa5f526
Калашникова Наталия (Kalashnikova_NA) <Kalashnikova_NA@directum.ru>
2019-05-22 11:31:53 +0400

    #82739. Создан тип документа для накладной MockWaybill. [Rx]

---
+++ src/Packages/Sungero.Capture/Sungero.Capture.Shared/MockWaybill/MockWaybillSharedFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Capture.MockWaybill;
+
+namespace Sungero.Capture.Shared
+{
+  partial class MockWaybillFunctions
+  {
+
+  }
+}
