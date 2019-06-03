6a88cfa83e777ef9efa7a423329b8e7df3ff122f
Дмитриев Иван (Dmitriev_IA) <Dmitriev_IA@directum.ru>
2019-06-03 10:15:28 +0400

    #83530. Акт. Псевдообязательность "Содержание" и "Контрагент". [Rx]

---
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/ContractStatement/ContractStatementHandlers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.SmartCapture.ContractStatement;
+
+namespace Sungero.SmartCapture
+{
+  partial class ContractStatementClientHandlers
+  {
+
+    public override void Refresh(Sungero.Presentation.FormRefreshEventArgs e)
+    {
+      base.Refresh(e);
+      
+      _obj.State.Properties.Subject.IsRequired = _obj.Info.Properties.Subject.IsRequired ||
+        (_obj.DocumentKind != null &&
+         (_obj.DocumentKind.NumberingType == Sungero.Docflow.DocumentKind.NumberingType.Registrable ||
+          _obj.DocumentKind.GenerateDocumentName == true));
+      
+      // Восстановить обязательность контрагента.
+      _obj.State.Properties.Counterparty.IsRequired = true;
+    }
+
+  }
+}
---
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/ContractStatement/ContractStatementSharedFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.SmartCapture.ContractStatement;
+
+namespace Sungero.SmartCapture.Shared
+{
+  partial class ContractStatementFunctions
+  {
+    public override void SetRequiredProperties()
+    {
+      base.SetRequiredProperties();
+      
+      // Содержание обязательно, только если это указано в метаданных.
+      _obj.State.Properties.Subject.IsRequired = _obj.Info.Properties.Subject.IsRequired;
+    }
+    
+    public override List<Sungero.Parties.ICounterparty> GetCounterparties()
+    {
+      if (_obj.Counterparty == null)
+        return null;
+        
+      return base.GetCounterparties();
+    }
+    
+    public override void FillName()
+    {
+      base.FillName();
+      
+      // Если имя формировать не из чего, то сформировать из краткого названия вида документа.
+      if (string.IsNullOrWhiteSpace(_obj.Name) ||
+          _obj.Name == Docflow.Resources.DocumentNameAutotext)
+        _obj.Name = _obj.DocumentKind.ShortName;
+    }
+  }
+}
