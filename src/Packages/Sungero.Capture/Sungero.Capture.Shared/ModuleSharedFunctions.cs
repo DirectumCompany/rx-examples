f7e8da843464dbdba4aba5f53d5637471a658ca1
Рассохина Мария (Rassokhina_MA) <Rassokhina_MA@directum.ru>
2019-06-11 10:27:40 +0400

    #83540 Удаление ConcatFullName()

--- src/Packages/Sungero.Capture/Sungero.Capture.Shared/ModuleSharedFunctions.cs
+++ src/Packages/Sungero.Capture/Sungero.Capture.Shared/ModuleSharedFunctions.cs
@@ -9,27 +9,6 @@ namespace Sungero.Capture.Shared
   public class ModuleFunctions
   {
     /// <summary>
-    /// Собрать ФИО из фамилии, имени и отчества.
-    /// </summary>
-    /// <param name="surname">Фамилия.</param>
-    /// <param name="name">Имя.</param>
-    /// <param name="patronymic">Отчество.</param>
-    /// <returns>ФИО.</returns>
-    public string ConcatFullName(string surname, string name, string patronymic)
-    {
-      var parts = new List<string>();
-
-      if (!string.IsNullOrWhiteSpace(surname))
-        parts.Add(surname);
-      if (!string.IsNullOrWhiteSpace(name))
-        parts.Add(name);
-      if (!string.IsNullOrWhiteSpace(patronymic))
-        parts.Add(patronymic);
-
-      return string.Join(" ", parts);
-    }
-    
-    /// <summary>
     /// Получить дату из строки с датой и временем.
     /// </summary>
     /// <param name="source">Исходная строка.</param>
