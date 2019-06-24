3b0453756a99667b061ea59cb3c231cc75725070
Колзин Артем (Kolzin_AA) <Kolzin_AA@directum.ru>
2019-06-24 10:58:07 +0400

    #81223 захват с почты шаг1.

--- src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/ModuleClientFunctions.cs
+++ src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/ModuleClientFunctions.cs
@@ -28,31 +28,42 @@ namespace Sungero.Capture.Client
       if (responsible == null)
         throw new ApplicationException(Resources.InvalidResponsibleId);
       
-      // Получить имена файлов.
-      var filePath = GetScannedPackagePath(filesInfo, folder);
-      var sourceFileName = GetScannedPackageName(instanceInfos);
-      if (string.IsNullOrEmpty(filePath))
-        throw new ApplicationException(Resources.FileNotFoundFormat(filePath));
-      
-      // Разделить пакет на документы.
-      Logger.DebugFormat("Begin of package \"{0}\" splitting and classification...", sourceFileName);
       var arioUrl = Functions.Module.Remote.GetArioUrl();
       if (string.IsNullOrEmpty(arioUrl))
         throw new ApplicationException(Resources.EmptyArioUrl);
       
-      var jsonClassificationResults = ProcessPackage(filePath, arioUrl, firstPageClassifierName, typeClassifierName);
-      Logger.DebugFormat("End of package \"{0}\" splitting and classification.", sourceFileName);
-      
-      // Принудительно обвалить захват, если Ario вернул ошибку. DCS запишет в лог и перезапустит процесс.
-      var ErrorMessage = ArioExtensions.ArioConnector.GetErrorMessageFromClassifyAndExtractFactsResult(jsonClassificationResults);
-      if (ErrorMessage != null && !string.IsNullOrWhiteSpace(ErrorMessage.Message))
-        throw new ApplicationException(ErrorMessage.Message);
-      
-      // Обработать пакет.
-      Logger.DebugFormat("Begin of splitted package \"{0}\" processing...", sourceFileName);
-      Functions.Module.Remote.ProcessSplitedPackage(sourceFileName, jsonClassificationResults, responsible);
-      Logger.DebugFormat("End of splitted package \"{0}\" processing.", sourceFileName);
-      Logger.Debug("End of captured package processing.");
+      var source = GetSourceType(deviceInfo);
+      var filePaths = new List<string>();
+      if (source == "mail")
+      {
+        filePaths = GetMailPackagePaths(filesInfo, folder);
+      }
+      else
+      {
+        filePaths = GetScannedPackagePath(filesInfo, folder);
+        if (!filePaths.Any())
+          throw new ApplicationException("Files not found");
+      }
+      // Получить имена файлов.      
+      foreach (var filePath in filePaths)
+      {
+        // Разделить пакет на документы.
+        var sourceFileName = System.IO.Path.GetFileName(filePath);
+        Logger.DebugFormat("Begin of package \"{0}\" splitting and classification...", sourceFileName);
+        var jsonClassificationResults = ProcessPackage(filePath, arioUrl, firstPageClassifierName, typeClassifierName);
+        Logger.DebugFormat("End of package \"{0}\" splitting and classification.", sourceFileName);
+        
+        // Принудительно обвалить захват, если Ario вернул ошибку. DCS запишет в лог и перезапустит процесс.
+        var ErrorMessage = ArioExtensions.ArioConnector.GetErrorMessageFromClassifyAndExtractFactsResult(jsonClassificationResults);
+        if (ErrorMessage != null && !string.IsNullOrWhiteSpace(ErrorMessage.Message))
+          throw new ApplicationException(ErrorMessage.Message);
+        
+        // Обработать пакет.
+        Logger.DebugFormat("Begin of splitted package \"{0}\" processing...", sourceFileName);
+        Functions.Module.Remote.ProcessSplitedPackage(filePath, jsonClassificationResults, responsible);
+        Logger.DebugFormat("End of splitted package \"{0}\" processing.", sourceFileName);
+        Logger.Debug("End of captured package processing.");
+      }     
     }
     
     /// <summary>
@@ -164,28 +175,86 @@ namespace Sungero.Capture.Client
     /// <param name="filesInfo">Путь к xml файлу DCS c информацией об импортируемых файлах.</param>
     /// <param name="folder">Путь к папке хранения файлов, переданных в пакете.</param>
     /// <returns>Путь к пакету документов со сканера.</returns>
-    public static string GetScannedPackagePath(string filesInfo, string folder)
+    public static List<string> GetScannedPackagePath(string filesInfo, string folder)
     {
       if (!File.Exists(filesInfo))
         throw new ApplicationException(Resources.NoFilesInfoInPackage);
       
+      var filePaths = new List<string>();
       var filesXDoc = System.Xml.Linq.XDocument.Load(filesInfo);
-      var fileElement = filesXDoc
+      var fileElements = filesXDoc
         .Element("InputFilesSection")
         .Element("Files")
-        .Elements()
-        .FirstOrDefault();
-      if (fileElement == null)
+        .Elements();
+        
+      if (!fileElements.Any())
         throw new ApplicationException(Resources.NoFilesInfoInPackage);
-      
-      var filePath = Path.Combine(folder, Path.GetFileName(fileElement.Element("FileName").Value));
-      if (!File.Exists(filePath))
+      foreach (var fileElement in fileElements)
       {
-        Logger.Error(Resources.FileNotFoundFormat(filePath));
-        return string.Empty;
+        var filePath = Path.Combine(folder, Path.GetFileName(fileElement.Element("FileName").Value));
+        if (!File.Exists(filePath))
+        {
+          Logger.Error(Resources.FileNotFoundFormat(filePath));
+        }
+        else
+        {
+          filePaths.Add(filePath);
+        }
+      }            
+      return filePaths;
+    }
+    
+    public static List<string> GetMailPackagePaths(string filesInfo, string folder)
+   {
+      var filePaths = new List<string>();
+      var filesXDoc = System.Xml.Linq.XDocument.Load(filesInfo);
+      if (filesXDoc == null)
+      {
+        Logger.Error(Resources.NoFilesInfoInPackage);
+        return filePaths;
       }
       
-      return filePath;
+      var fileElements = filesXDoc.Element("InputFilesSection").Element("Files").Elements();
+      
+      var mailBodyElements = fileElements.Where(e => e.Element("FileDescription").Value.Equals("body.txt", StringComparison.InvariantCultureIgnoreCase) ||
+                                                e.Element("FileDescription").Value.Equals("body.html", StringComparison.InvariantCultureIgnoreCase));
+      
+      if(mailBodyElements.Any())
+      {
+        foreach (var mailBodyElement in mailBodyElements)
+        {
+          var fileDescription = mailBodyElement.Element("FileDescription").Value;
+          if (Sungero.Content.Shared.ElectronicDocumentUtils.GetAssociatedApplication(fileDescription) != null)
+          {
+            filePaths.Add(Path.Combine(folder, Path.GetFileName(mailBodyElement.Element("FileName").Value)));
+            break;
+          }
+        }
+      }
+      
+      var fileAttachments = fileElements.Where(f => !mailBodyElements.Contains(f));
+      foreach (var fileAttachment in fileAttachments)
+      {
+        var fileDescription = fileAttachment.Element("FileDescription").Value;
+        
+        //Если для файла нет приложения-обработчика, то платформа не даст создать документ.
+        var application = Sungero.Content.Shared.ElectronicDocumentUtils.GetAssociatedApplication(fileDescription);
+        if (application == null)
+          continue;
+        
+        // Отбрасываем изображения из тела письма (например картинки из подписей).
+        if (System.Text.RegularExpressions.Regex.IsMatch(fileDescription, @"^ATT\d+\s\d+\.\w+"))
+          continue;
+        
+        var filePath = Path.Combine(folder, Path.GetFileName(fileAttachment.Element("FileName").Value));
+        if (!File.Exists(filePath))
+        {
+          Logger.Error(Resources.FileNotFoundFormat(filePath));
+          continue;
+        }
+        filePaths.Add(filePath);
+      }
+      return filePaths;
     }
     
     /// <summary>
@@ -213,10 +282,14 @@ namespace Sungero.Capture.Client
     
     /// <summary>
     /// Установить цвет у распознанных свойств в карточке документа.
+    /// Получить имя пакета документов со сканера.
     /// </summary>
     /// <param name="document">Документ.</param>
     [Public]
     public virtual void SetPropertiesColors(Sungero.Docflow.IOfficialDocument document)
+    /// <param name="instanceInfos">Путь к xml файлу DCS c информацией об экземплярах захвата и о захваченных файлах.</param>
+    /// <returns>Путь к пакету документов со сканера.</returns>
+    public static string GetSourceType(string deviceInfo)
     {
       // Точно распознанные свойства документа подсветить зелёным цветом, неточно - жёлтым.
       // Точно и неточно распознанные свойства получить с сервера отдельными вызовами метода из-за ограничений платформы.
@@ -226,6 +299,14 @@ namespace Sungero.Capture.Client
       var notExactlyRecognizedProperties = Sungero.Capture.PublicFunctions.Module.Remote.GetRecognizedDocumentProperties(document, false);
       HighlightProperties(document, notExactlyRecognizedProperties, Sungero.Core.Colors.Highlights.Yellow);
     }
+      if (!File.Exists(deviceInfo))
+        throw new ApplicationException(Resources.NoFilesInfoInPackage);  
+      
+      var filesXDoc = System.Xml.Linq.XDocument.Load(deviceInfo);
+      var element = filesXDoc.Element("MailSourceInfo");
+      if (element != null)
+        return "mail";
+      return "folder";      
     
     /// <summary>
     /// Подсветить указанные свойства в карточке документа.
