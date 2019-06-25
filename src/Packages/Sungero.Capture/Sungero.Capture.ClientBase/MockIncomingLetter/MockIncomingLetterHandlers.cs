a5e92d790fdd5dce8d85dd37553998a9c6294c74
Калашникова Наталия (Kalashnikova_NA) <Kalashnikova_NA@directum.ru>
2019-06-25 09:16:20 +0400

    #84645 Сброс подсветки в MockIncomingLetter при изменении полей + вернула выравнивание в IncomingLetter

--- src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/MockIncomingLetter/MockIncomingLetterHandlers.cs
+++ src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/MockIncomingLetter/MockIncomingLetterHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,87 @@ namespace Sungero.Capture
 {
   partial class MockIncomingLetterClientHandlers
   {
+
+    public virtual void AddresseesValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.Addressees.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void RecipientTrrcValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.RecipientTrrc.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void RecipientTinValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.RecipientTin.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void RecipientValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.Recipient.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void ContactValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.Contact.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void SignatoryValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.Signatory.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void InResponseToValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.InResponseTo.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void InNumberValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.InNumber.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void DatedValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.Dated.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void CorrespondentTrrcValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.CorrespondentTrrc.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void CorrespondentTinValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.CorrespondentTin.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public virtual void CorrespondentValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      this._obj.State.Properties.Correspondent.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public override void SubjectValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      base.SubjectValueInput(e);
+      
+      this._obj.State.Properties.Subject.HighlightColor = Sungero.Core.Colors.Highlights.Empty;
+    }
+
+    public override void Refresh(Sungero.Presentation.FormRefreshEventArgs e)
+    {
+      base.Refresh(e);
+      
+      Sungero.Capture.PublicFunctions.Module.SetPropertiesColors(_obj);
+    }
+
+    public override void Showing(Sungero.Presentation.FormShowingEventArgs e)
+    {
+      base.Showing(e);
+      
+      Sungero.Capture.PublicFunctions.Module.SetPropertiesColors(_obj);
+    }
 
   }
 }
