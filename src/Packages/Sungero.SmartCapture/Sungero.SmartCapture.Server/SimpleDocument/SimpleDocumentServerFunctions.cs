202aaeee2ac43248c30141c152088c52661fe6c2
Колзин Артем (Kolzin_AA) <Kolzin_AA@directum.ru>
2019-07-04 17:03:49 +0400

    #85012 предпросмотр в простом документе

---
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Server/SimpleDocument/SimpleDocumentServerFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.SmartCapture.SimpleDocument;
+
+namespace Sungero.SmartCapture.Server
+{
+  partial class SimpleDocumentFunctions
+  {
+
+  }
+}
---
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/SimpleDocument/SimpleDocumentConstants.cs
@@ -0,0 +1,10 @@
+using System;
+using Sungero.Core;
+
+namespace Sungero.SmartCapture.Constants.Docflow
+{
+  public static class SimpleDocument
+  {
+
+  }
+}
