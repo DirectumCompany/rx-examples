b9641e41d007e81ce5a6c1a912179ddc636a9a60
Калашникова Наталия (Kalashnikova_NA) <Kalashnikova_NA@directum.ru>
2019-10-07 11:08:20 +0400

    #89905. Перекрыты доп.соглашения и добавлены поля на вкладку верификации.

---
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Server/SupAgreement/SupAgreementServerFunctions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.SmartCapture.SupAgreement;
+
+namespace Sungero.SmartCapture.Server
+{
+  partial class SupAgreementFunctions
+  {
+
+  }
+}
