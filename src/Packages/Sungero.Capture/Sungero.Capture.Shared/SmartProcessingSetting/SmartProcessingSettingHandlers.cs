167fedaf9b77d20d759cbed67d3463bf70387741
Котегов Валентин (Kotegov_VS) <Kotegov_VS@directum.ru>
2019-11-11 11:18:33 +0400

    #91791 Добавил автотрим пробелов в адресе Ario

--- src/Packages/Sungero.Capture/Sungero.Capture.Shared/SmartProcessingSetting/SmartProcessingSettingHandlers.cs
+++ src/Packages/Sungero.Capture/Sungero.Capture.Shared/SmartProcessingSetting/SmartProcessingSettingHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -9,6 +9,17 @@ namespace Sungero.Capture
 {
   partial class SmartProcessingSettingSharedHandlers
   {
+    public virtual void ArioUrlChanged(Sungero.Domain.Shared.StringPropertyChangedEventArgs e)
+    {
+      if (e.NewValue == e.OldValue || e.NewValue == null)
+        return;
+      
+      var trimedArioUrl = e.NewValue.Trim(' ');
+      if (e.NewValue == trimedArioUrl)
+        return;
+      
+      _obj.ArioUrl = trimedArioUrl;
+    }
 
   }
 }
