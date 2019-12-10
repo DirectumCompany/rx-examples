1948f5509852428363a1f6093ac1f3be2a218f1e
Рассохина Мария (Rassokhina_MA) <Rassokhina_MA@directum.ru>
2019-12-10 14:42:37 +0400

    #93633: Изменено поведение обязательности категории, содержания, контрагента в договоре. После рецензирования.

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/Contract/ContractSharedFunctions.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/Contract/ContractSharedFunctions.cs
@@ -15,17 +15,22 @@ namespace Sungero.SmartCapture.Shared
       base.SetRequiredProperties();
       
       // Изменить обязательность полей в зависимости от того, программная или визульная работа.
-      var documentParams = ((Domain.Shared.IExtendedEntity)_obj).Params;
-      if (documentParams.ContainsKey(Capture.PublicConstants.Module.IsVisualModeParamName))
+      var isVisualMode = ((Domain.Shared.IExtendedEntity)_obj).Params.ContainsKey(Capture.PublicConstants.Module.IsVisualModeParamName);
+      if (isVisualMode)
+      {
+        // При визуальной работе обязательность содержания и контрагента как в Contract.
+        // Обязательность категории вычисляется по стандартной логике.
         _obj.State.Properties.Subject.IsRequired = true;
+        _obj.State.Properties.Counterparty.IsRequired = true;
+      }
       else
       {
-        // Содержание, категория и контрагент обязательны, только если это указано в метаданных.
-        _obj.State.Properties.Subject.IsRequired = _obj.Info.Properties.Subject.IsRequired;
-        _obj.State.Properties.DocumentGroup.IsRequired = _obj.Info.Properties.DocumentGroup.IsRequired;
-        _obj.State.Properties.Counterparty.IsRequired = _obj.Info.Properties.Counterparty.IsRequired;
+        // При программной работе содержание, контрагента и категорию делаем необязательными.
+        // Чтобы сбросить обязательность, если она изменилась в вызове текущего метода в базовой сущности.
+        _obj.State.Properties.Subject.IsRequired = false;
+        _obj.State.Properties.Counterparty.IsRequired = false;
+        _obj.State.Properties.DocumentGroup.IsRequired = false;
       }
-      
     }
     
     public override void FillName()
