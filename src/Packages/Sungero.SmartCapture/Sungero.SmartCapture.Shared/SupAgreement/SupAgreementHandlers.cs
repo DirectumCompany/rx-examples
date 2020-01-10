e7a765d33c3cdcc2eb9acf63b8f9b39923754291
Калашникова Наталия (Kalashnikova_NA) <Kalashnikova_NA@directum.ru>
2020-01-10 16:43:40 +0400

    Исправление раскраски в договоре после демо.

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/SupAgreement/SupAgreementHandlers.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/SupAgreement/SupAgreementHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -10,5 +10,12 @@ namespace Sungero.SmartCapture
   partial class SupAgreementSharedHandlers
   {
 
+    public override void CounterpartySignatoryChanged(Sungero.Docflow.Shared.ContractualDocumentBaseCounterpartySignatoryChangedEventArgs e)
+    {
+      base.CounterpartySignatoryChanged(e);
+      
+      this._obj.State.Properties.CounterpartySignatory.HighlightColor = Sungero.Core.Colors.Empty;
+    }
   }
+
 }
