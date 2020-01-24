44eb1fb0fbfeeb5b311bc7fff89af1684dead784
Дмитриев Иван (Dmitriev_IA) <Dmitriev_IA@directum.ru>
2020-01-24 15:06:10 +0400

    #96039. SC игнорировать изменение номера/даты/журнала при регистрации/присвоении номера в процессе верификации

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/ModuleClientFunctions.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/ModuleClientFunctions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -8,6 +8,17 @@ namespace Sungero.SmartCapture.Client
 {
   public class ModuleFunctions
   {
-
+    /// <summary>
+    /// Удалить параметр NeedValidateRegisterFormat.
+    /// </summary>
+    /// <param name="document">Документ.</param>
+    /// <param name="e">Аргумент действия.</param>
+    public static void RemoveNeedValidateRegisterFormatParameter(Sungero.Docflow.IOfficialDocument document,
+                                                                 Sungero.Domain.Client.ExecuteActionArgs e)
+    {
+      // Если документ в процессе верификации, то игнорировать изменение полей регистрационных данных.
+      if (document.VerificationState == Sungero.Docflow.OfficialDocument.VerificationState.InProcess)
+        e.Params.Remove(Sungero.Docflow.Constants.OfficialDocument.NeedValidateRegisterFormat);
+    }
   }
 }
