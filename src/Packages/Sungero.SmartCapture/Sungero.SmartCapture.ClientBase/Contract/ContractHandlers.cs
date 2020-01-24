aa4cb1455ac0bf04e6e07893cf9fda08766128e9
Сулейманов Рустам (Suleymanov_RA) <Suleymanov_RA@directum.ru>
2020-01-24 11:13:25 +0400

    #95437 снятие подсветки с полей № и дата в договорах и допиках после смены значения.

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/Contract/ContractHandlers.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/Contract/ContractHandlers.cs
@@ -10,6 +10,20 @@ namespace Sungero.SmartCapture
   partial class ContractClientHandlers
   {
 
+    public override void RegistrationDateValueInput(Sungero.Presentation.DateTimeValueInputEventArgs e)
+    {
+      base.RegistrationDateValueInput(e);
+
+      this._obj.State.Properties.RegistrationDate.HighlightColor = Sungero.Core.Colors.Empty;
+    }
+
+    public override void RegistrationNumberValueInput(Sungero.Presentation.StringValueInputEventArgs e)
+    {
+      base.RegistrationNumberValueInput(e);
+      
+      this._obj.State.Properties.RegistrationNumber.HighlightColor = Sungero.Core.Colors.Empty;
+    }
+
     public override void NoteValueInput(Sungero.Presentation.StringValueInputEventArgs e)
     {
       base.NoteValueInput(e);
