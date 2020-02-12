3050277cdc1771ba7d9fcafebdee0d8848ccc3f3
Шишкова Анна (Shishkova_AM) <Shishkova_AM@directum.ru>
2020-02-12 09:11:39 +0400

    Correct syntax mssql

--- src/Packages/Sungero.Capture/VersionData/3.3.15.0/Script/convert_after_mssql.sql
+++ src/Packages/Sungero.Capture/VersionData/3.3.15.0/Script/convert_after_mssql.sql
@@ -1,54 +1,48 @@
-﻿﻿--Полностью очистить таблицы EntityRecognitionInfo, данные в них на момент конвертации не имеют ценности.
-delete from sungero_commons_facts;
-delete from sungero_commons_entityrecogn;
+﻿--Заполнить таблицы EntityRecognitionInfo на основе таблиц DocumentRecognitionInfo.
+select drr.Id
+      , '32EA0857-ADF7-41C2-BC0C-188320E40786' as Discriminator
+      , drr.SecureObject
+      , drr.Status
+      , drr.Name
+      , drr.RecognClass
+      , drr.Probability
+      , drr.DocumentId 
+      , edoc.Discriminator as entitytype
+from Sungero_Capture_DocumentRecogn as drr
+join Sungero_Content_Edoc as edoc
+on drr.DocumentId = edoc.Id;
 
---Заполнить таблицы EntityRecognitionInfo на основе таблиц DocumentRecognitionInfo.
-insert into sungero_commons_entityrecogn
-select drr.id
-      , '32EA0857-ADF7-41C2-BC0C-188320E40786' as discriminator
-      , drr.secureobject
-      , drr.status
-      , drr.name
-      , drr.recognclass
-      , drr.probability
-      , drr.documentid 
-      , edoc.discriminator as entitytype
-from sungero_capture_documentrecogn as drr
-join sungero_content_edoc as edoc
-on drr.documentid = edoc.id;
-
-insert into sungero_commons_facts
-select  id
-      , 'EA588697-56D5-4A14-9A78-39FA6517351B' as discriminator
-      , documentrecogn
-      , factid
-      , fieldid
-      , propertyname
-      , propertyvalue
-      , factname
-      , fieldname
-      , fieldvalue
-      , fieldprobabil
-      , position
-      , istrusted
-      , verifiedvalue
-      , factlabel
-      , collectionreco
-from sungero_capture_facts;
+select Id
+      , 'EA588697-56D5-4A14-9A78-39FA6517351B' as Discriminator
+      , DocumentRecogn
+      , FactId
+      , FieldId
+      , PropertyName
+      , PropertyValue
+      , FactName
+      , FieldName
+      , FieldValue
+      , FieldProbabil
+      , Position
+      , IsTrusted
+      , VerifiedValue
+      , FactLabel
+      , CollectionReco
+from Sungero_Capture_Facts;
 
 --Актуализировать в sungero_system_ids текущие ИД таблиц EntityRecognitionInfo.
-delete from sungero_system_ids
-where tablename = 'Sungero_Commons_EntityRecogn';
+delete from Sungero_System_Ids
+where TableName = 'Sungero_Commons_EntityRecogn';
 
 if 
-  ((select count(id) from sungero_commons_entityrecogn) > 0) 
-  insert into sungero_system_ids
-  values('Sungero_Commons_EntityRecogn', (select max(id) from sungero_commons_entityrecogn));
-    
-delete from sungero_system_ids
-where tablename = 'Sungero_Commons_Facts';
+  ((select count(Id) from Sungero_Commons_EntityRecogn) > 0) 
+  insert into Sungero_System_Ids
+  values('Sungero_Commons_EntityRecogn', (select max(Id) from Sungero_Commons_EntityRecogn));
+
+delete from Sungero_System_Ids
+where TableName = 'Sungero_Commons_Facts';
 
 if 
-  ((select count(id) from sungero_commons_facts) > 0)
-  insert into sungero_system_ids
-  values('Sungero_Commons_Facts', (select max(id) from sungero_commons_facts));
+  ((select count(Id) from Sungero_Commons_Facts) > 0)
+  insert into Sungero_System_Ids
+  values('Sungero_Commons_Facts', (select max(Id) from Sungero_Commons_Facts));
