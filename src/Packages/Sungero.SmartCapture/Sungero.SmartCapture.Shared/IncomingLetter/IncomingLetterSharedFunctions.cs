6b0a02f555cb12c94df124b9668180b463a58f2d
Сулейманов Рустам (Suleymanov_RA) <Suleymanov_RA@directum.ru>
2020-02-21 11:08:27 +0400

    #97293 #97651 Перенос вход. письма в коробку (псевдообязательность)

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/IncomingLetter/IncomingLetterSharedFunctions.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.Shared/IncomingLetter/IncomingLetterSharedFunctions.cs
@@ -8,20 +8,6 @@ using Sungero.SmartCapture.IncomingLetter;
 namespace Sungero.SmartCapture.Shared
 {
   partial class IncomingLetterFunctions
-  {
-    public override void SetRequiredProperties()
-    {
-      base.SetRequiredProperties();
-
-      // Изменить обязательность полей в зависимости от того, программная или визульная работа.
-      var isVisualMode = ((Domain.Shared.IExtendedEntity)_obj).Params.ContainsKey(Sungero.Docflow.PublicConstants.OfficialDocument.IsVisualModeParamName);
-
-      // При визуальной работе обязательность содержания и корреспондента как в IncomingLetter.
-      // При программной работе содержание и корреспондент - необязательные.
-      // Чтобы сбросить обязательность, если она изменилась в вызове текущего метода в базовой сущности.
-      _obj.State.Properties.Subject.IsRequired = isVisualMode;
-      _obj.State.Properties.Correspondent.IsRequired = isVisualMode;
-    }
-    
+  {    
   }
 }
