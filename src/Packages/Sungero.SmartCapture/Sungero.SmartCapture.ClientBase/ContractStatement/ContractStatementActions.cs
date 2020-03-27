f6db4d8fa4d9dad7d3501251fbee6c013da202a7
Сулейманов Рустам (Suleymanov_RA) <Suleymanov_RA@directum.ru>
2020-03-27 16:19:45 +0400

    #99837 #100078 перенос действий AssignNumber и Register в коробку

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/ContractStatement/ContractStatementActions.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/ContractStatement/ContractStatementActions.cs
@@ -7,54 +7,5 @@ using Sungero.SmartCapture.ContractStatement;
 
 namespace Sungero.SmartCapture.Client
 {
-  partial class ContractStatementActions
-  {
-    public override void AssignNumber(Sungero.Domain.Client.ExecuteActionArgs e)
-    {
-      // Bug 96201
-      // Сброс подсветки полей происходит в событии контрола InputValue.
-      // Действия регистрации изменяют значения полей програмно, событие ValueInput не вызывается.
-      var originalNumber = _obj.State.Properties.RegistrationNumber.OriginalValue;
-      var originalDate = _obj.State.Properties.RegistrationDate.OriginalValue;
-      
-      Sungero.SmartCapture.Functions.Module.RemoveNeedValidateRegisterFormatParameter(_obj, e);
-      
-      base.AssignNumber(e);
-      
-      if(_obj.RegistrationNumber != originalNumber)
-        _obj.State.Properties.RegistrationNumber.HighlightColor = Colors.Empty;
-      if(_obj.RegistrationDate != originalDate)
-        _obj.State.Properties.RegistrationDate.HighlightColor = Colors.Empty;
-    }
-
-    public override bool CanAssignNumber(Sungero.Domain.Client.CanExecuteActionArgs e)
-    {
-      return base.CanAssignNumber(e);
-    }
-
-    public override void Register(Sungero.Domain.Client.ExecuteActionArgs e)
-    {
-      // Bug 96201
-      // Сброс подсветки полей происходит в событии контрола InputValue.
-      // Действия регистрации изменяют значения полей програмно, событие ValueInput не вызывается.
-      var originalNumber = _obj.State.Properties.RegistrationNumber.OriginalValue;
-      var originalDate = _obj.State.Properties.RegistrationDate.OriginalValue;
-      
-      Sungero.SmartCapture.Functions.Module.RemoveNeedValidateRegisterFormatParameter(_obj, e);
-      
-      base.Register(e);
-      
-      if(_obj.RegistrationNumber != originalNumber)
-        _obj.State.Properties.RegistrationNumber.HighlightColor = Colors.Empty;
-      if(_obj.RegistrationDate != originalDate)
-        _obj.State.Properties.RegistrationDate.HighlightColor = Colors.Empty;
-    }
-
-    public override bool CanRegister(Sungero.Domain.Client.CanExecuteActionArgs e)
-    {
-      return base.CanRegister(e);
-    }
-
-  }
 
 }
--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/ModuleClientFunctions.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/ModuleClientFunctions.cs
@@ -7,25 +7,6 @@ using Sungero.CoreEntities;
 namespace Sungero.SmartCapture.Client
 {
   public class ModuleFunctions
-  {
-    /* Dmitriev_IA:
-     * RemoveNeedValidateRegisterFormatParameter() расположена на уровне решения для использования в перекрытых типах.
-     * При размещении RemoveNeedValidateRegisterFormatParameter() в модуле Capture необходимо будет указывать атрибут [Public]
-     * для использования в перекрытых типах.
-     * Namespace Sungero.Domain.Client недоступен в g.cs при указании атрибута [Public]. Использование using не спасает.
-     */
-    
-    /// <summary>
-    /// Удалить параметр NeedValidateRegisterFormat.
-    /// </summary>
-    /// <param name="document">Документ.</param>
-    /// <param name="e">Аргумент действия.</param>
-    public static void RemoveNeedValidateRegisterFormatParameter(Sungero.Docflow.IOfficialDocument document,
-                                                                 Sungero.Domain.Client.ExecuteActionArgs e)
-    {
-      // Если документ в процессе верификации, то игнорировать изменение полей регистрационных данных.
-      if (document.VerificationState == Sungero.Docflow.OfficialDocument.VerificationState.InProcess)
-        e.Params.Remove(Sungero.Docflow.Constants.OfficialDocument.NeedValidateRegisterFormat);
-    }
+  {    
   }
 }
