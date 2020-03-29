8488889b1503c5c948fd9e911e4e3e748c46a7ca
Сулейманов Рустам (Suleymanov_RA) <Suleymanov_RA@directum.ru>
2020-03-29 16:11:02 +0400

    #99837 #100078 снятие раскраски при смене значения свойств для IncomingInvoice

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/IncomingInvoice/IncomingInvoiceHandlers.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/IncomingInvoice/IncomingInvoiceHandlers.cs
@@ -7,29 +7,4 @@ using Sungero.SmartCapture.IncomingInvoice;
 
 namespace Sungero.SmartCapture
 {
-  partial class IncomingInvoiceClientHandlers
-  {
-
-    public override void ContractValueInput(Sungero.Contracts.Client.IncomingInvoiceContractValueInputEventArgs e)
-    {
-      base.ContractValueInput(e);
-      
-      this._obj.State.Properties.Contract.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-    public override void DateValueInput(Sungero.Presentation.DateTimeValueInputEventArgs e)
-    {
-      base.DateValueInput(e);
-      
-      this._obj.State.Properties.Date.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-    public override void NumberValueInput(Sungero.Presentation.StringValueInputEventArgs e)
-    {
-      base.NumberValueInput(e);
-      
-      this._obj.State.Properties.Number.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-  }
 }
