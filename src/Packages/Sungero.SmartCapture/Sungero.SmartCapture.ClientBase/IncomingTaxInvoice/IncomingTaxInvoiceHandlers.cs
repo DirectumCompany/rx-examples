9b658ab751ee87792ee75ebab4e35f8c744ab1bf
Сулейманов Рустам (Suleymanov_RA) <Suleymanov_RA@directum.ru>
2020-03-29 16:31:17 +0400

    #99837 #100078 снятие раскраски при смене значения свойств для IncomingTaxInvoice

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/IncomingTaxInvoice/IncomingTaxInvoiceHandlers.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/IncomingTaxInvoice/IncomingTaxInvoiceHandlers.cs
@@ -7,32 +7,4 @@ using Sungero.SmartCapture.IncomingTaxInvoice;
 
 namespace Sungero.SmartCapture
 {
-  partial class IncomingTaxInvoiceClientHandlers
-  {
-
-    public override void CorrectedValueInput(Sungero.Docflow.Client.AccountingDocumentBaseCorrectedValueInputEventArgs e)
-    {
-      base.CorrectedValueInput(e);
-      this._obj.State.Properties.Corrected.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-    public override void IsAdjustmentValueInput(Sungero.Presentation.BooleanValueInputEventArgs e)
-    {
-      base.IsAdjustmentValueInput(e);
-      this._obj.State.Properties.IsAdjustment.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-    public override void CurrencyValueInput(Sungero.Docflow.Client.AccountingDocumentBaseCurrencyValueInputEventArgs e)
-    {
-      base.CurrencyValueInput(e);
-      this._obj.State.Properties.Currency.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-    public override void RegistrationNumberValueInput(Sungero.Presentation.StringValueInputEventArgs e)
-    {
-      base.RegistrationNumberValueInput(e);
-      this._obj.State.Properties.RegistrationNumber.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-  }
 }
