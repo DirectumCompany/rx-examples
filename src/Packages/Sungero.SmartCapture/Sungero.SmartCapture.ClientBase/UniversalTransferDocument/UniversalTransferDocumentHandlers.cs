868fcb51e2ed9df99ff7d09129b2fc5cb2614f09
Сулейманов Рустам (Suleymanov_RA) <Suleymanov_RA@directum.ru>
2020-03-29 16:44:13 +0400

    #99837 #100078 снятие раскраски при смене значения свойств для УПД

--- src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/UniversalTransferDocument/UniversalTransferDocumentHandlers.cs
+++ src/Packages/Sungero.SmartCapture/Sungero.SmartCapture.ClientBase/UniversalTransferDocument/UniversalTransferDocumentHandlers.cs
@@ -7,22 +7,4 @@ using Sungero.SmartCapture.UniversalTransferDocument;
 
 namespace Sungero.SmartCapture
 {
-  partial class UniversalTransferDocumentClientHandlers
-  {
-
-    public override void RegistrationNumberValueInput(Sungero.Presentation.StringValueInputEventArgs e)
-    {
-      base.RegistrationNumberValueInput(e);
-      
-      this._obj.State.Properties.RegistrationNumber.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-    public override void CorrectedValueInput(Sungero.Docflow.Client.AccountingDocumentBaseCorrectedValueInputEventArgs e)
-    {
-      base.CorrectedValueInput(e);
-      
-      this._obj.State.Properties.Corrected.HighlightColor = Sungero.Core.Colors.Empty;
-    }
-
-  }
 }
