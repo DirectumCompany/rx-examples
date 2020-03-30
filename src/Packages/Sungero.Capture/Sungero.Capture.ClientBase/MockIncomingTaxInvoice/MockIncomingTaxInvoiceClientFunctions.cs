6f9e13aea8ec733709c14ed8c8689469959be899
Сулейманов Рустам (Suleymanov_RA) <Suleymanov_RA@directum.ru>
2020-03-30 17:08:09 +0400

    #99837 #100018 поменял имя метода

--- src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/MockIncomingTaxInvoice/MockIncomingTaxInvoiceClientFunctions.cs
+++ src/Packages/Sungero.Capture/Sungero.Capture.ClientBase/MockIncomingTaxInvoice/MockIncomingTaxInvoiceClientFunctions.cs
@@ -23,10 +23,10 @@ namespace Sungero.Capture.Client
     /// </summary>
     /// <param name="documentRecognitionInfo">Результат распознавания документа.</param>
     /// <param name="highlightActivationStyle">Параметры отображения фокусировки подсветки.</param>
-    public override void AdditionalHighlight(Commons.IEntityRecognitionInfo documentRecognitionInfo,
+    public override void SetAdditionalHighlight(Commons.IEntityRecognitionInfo documentRecognitionInfo,
                                              Docflow.Structures.Module.IHighlightActivationStyle highlightActivationStyle)
     {
-      base.AdditionalHighlight(documentRecognitionInfo, highlightActivationStyle);
+      base.SetAdditionalHighlight(documentRecognitionInfo, highlightActivationStyle);
       
       // Подсветка номенклатуры.
       var incomingTaxInvoice = MockIncomingTaxInvoices.As(_obj);
