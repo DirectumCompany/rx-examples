16b86b9a4ff5ed43dccf75e1c4705ee4b7439669
Афанасьева Елизавета (Afanaseva_ElV) <Afanaseva_ElV@directum.ru>
2023-01-17 14:54:44 +0400

    Выдедлили метод для добавления штампов на страницу, константы

--- src/Packages/Sungero.Examples/Sungero.Examples.Isolated/Sungero.Docflow/IsolatedAreas/PdfConverter/IsolatedFunctions.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.Isolated/Sungero.Docflow/IsolatedAreas/PdfConverter/IsolatedFunctions.cs
@@ -10,44 +10,69 @@ namespace Sungero.Examples.Module.Docflow.Isolated.PdfConverter
 {
   public partial class IsolatedFunctions
   {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="inputStream"></param>
+    /// <param name="htmlStamps"></param>
+    /// <param name="extension"></param>
+    /// <returns></returns>
     [Public]
     public virtual Stream AddAllSignatureStamps(Stream inputStream, List<string> htmlStamps, string extension)
     {
       System.IO.Stream outputStream = null;
-      var pdfDocumentStream = new MemoryStream();
+      
       try
       {
-        var pdfConverter = new AsposeExtensions.Converter();
-        inputStream.CopyTo(pdfDocumentStream);
-        var pdfDocument = pdfConverter.GeneratePdfDocument(pdfDocumentStream, extension);
-        // Координаты отсчитываются от нижнего левого угла.
-        var horizontalCoord = 312;
-        var verticalCoord = pdfDocument.Pages[1].Rect.Height - 100;
-        // Отметка об ЭП проставляется только на первой странице.
-        var pages = new int[] { 1 };
-        
-        foreach (var htmlStamp in htmlStamps)
+        using (var pdfDocumentStream = new MemoryStream())
         {
-          var pdfStamp = pdfConverter.CreateMarkFromHtml(htmlStamp);
-          pdfStamp.XIndent = horizontalCoord;
-          // Отступ сверху на высоту штампа.
-          pdfStamp.YIndent = verticalCoord - pdfStamp.PdfPage.PageInfo.Height;
-          outputStream = pdfConverter.GetPdfDocumentWithStamp(pdfDocument, pdfStamp, pages, false);
-          verticalCoord = verticalCoord - pdfStamp.PdfPage.PageInfo.Height - 5;
+          var pdfConverter = new AsposeExtensions.Converter();
+          inputStream.CopyTo(pdfDocumentStream);
+          var pdfDocument = pdfConverter.GeneratePdfDocument(pdfDocumentStream, extension);
+          outputStream  = this.AddHtmlStamps(pdfDocument, pdfConverter, htmlStamps, outputStream);
         }
-        pdfDocument.Save(pdfDocumentStream);
         return outputStream;
       }
       catch (Exception ex)
       {
-        outputStream.Close();
+        outputStream?.Close();
         Logger.Error("Cannot add stamp", ex);
         throw new AppliedCodeException("Cannot add stamp");
       }
-      finally
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="pdfDocument"></param>
+    /// <param name="pdfConverter"></param>
+    /// <param name="htmlStamps"></param>
+    /// <param name="outputStream"></param>
+    /// <returns></returns>
+    public virtual Stream AddHtmlStamps(Aspose.Pdf.Document pdfDocument, Sungero.AsposeExtensions.Converter pdfConverter, List<string> htmlStamps, Stream outputStream)
+    {
+      const int horizontalCoord = 312;
+      const int verticalOffset = 100;
+      const int verticalSpacing = 5;
+      
+      // Координаты отсчитываются от нижнего левого угла.
+      var verticalCoord = pdfDocument.Pages[1].Rect.Height - verticalOffset;
+      
+      // Отметка об ЭП проставляется только на первой странице.
+      var pages = new int[] { 1 };
+      
+      foreach (var htmlStamp in htmlStamps)
       {
-        pdfDocumentStream.Close();
+        var pdfStamp = pdfConverter.CreateMarkFromHtml(htmlStamp);
+        pdfStamp.XIndent = horizontalCoord;
+        
+        // Отступ сверху на высоту штампа.
+        pdfStamp.YIndent = verticalCoord - pdfStamp.PdfPage.PageInfo.Height;
+        outputStream = pdfConverter.GetPdfDocumentWithStamp(pdfDocument, pdfStamp, pages, false);
+        verticalCoord = verticalCoord - pdfStamp.PdfPage.PageInfo.Height - verticalSpacing;
       }
+      
+      return outputStream;
     }
   }
 }
