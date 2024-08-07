297d10daaf050248f1b59b09d90e0c202c5f3954
Шевелев Антон <shevelev_aa@directum.ru>
2024-08-07 15:55:05 +0400

    ребейз

---
+++ src/Packages/Sungero.Examples/Sungero.Examples.Shared/OfficialDocument/OfficialDocumentHandlers.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+using Sungero.Examples.OfficialDocument;
+
+namespace Sungero.Examples
+{
+  partial class OfficialDocumentSharedHandlers
+  {
+
+  }
+}
