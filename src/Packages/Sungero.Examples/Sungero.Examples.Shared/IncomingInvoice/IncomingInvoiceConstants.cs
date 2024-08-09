4021d389d9e012edf90c48d781784b88a9ba0366
somatan <101842732+somatan@users.noreply.github.com>
2024-08-09 17:08:16 +0400

    User Story 348416: PDF. Examples. Входящий счет. Оплата. (#19)
    #348416 Добавлен пример перекрытия для простановки отметки "Оплачено" для вх. счетов

--- src/Packages/Sungero.Examples/Sungero.Examples.Shared/IncomingInvoice/IncomingInvoiceConstants.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.Shared/IncomingInvoice/IncomingInvoiceConstants.cs
@@ -5,6 +5,16 @@ namespace Sungero.Examples.Constants.Contracts
 {
   public static class IncomingInvoice
   {
-
+    // Sid отметки "Оплачено".
+    [Public]
+    public const string PaymentMarkKindSid = "A89D9F46-9065-422A-B6CA-DED9D29EEDC7";
+    
+    // Полное имя класса, из которого вызывается метод получения отметки "Оплачено".
+    [Public]
+    public const string PaymentMarkKindClass = "Sungero.Examples.Server.IncomingInvoiceFunctions";
+    
+    // Имя метода получения отметки "Оплачено".
+    [Public]
+    public const string PaymentMarkKindMethod = "GetPaymentMarkAsHtml";
   }
 }
