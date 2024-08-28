c4925d76731d1abf14da8be705ff00b27bc2a793
Kovalev-MK <159262906+Kovalev-MK@users.noreply.github.com>
2024-08-28 13:03:11 +0400

    User Story 348175: PDF. Examples. Простановка отметок об ЭП на служебной записке (#23)
    * rebase
    
    * rebase
    
    * #348175 Rename
    
    * #348175 Ревью
    
    * #348175 Ревью
    
    * #348175 Ревью
    
    * #348175 Вынесен ToString
    
    * #348175 Rebase

--- src/Packages/Sungero.Examples/Sungero.Examples.Server/Sungero.Docflow/ModuleInitializer.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.Server/Sungero.Docflow/ModuleInitializer.cs
@@ -23,6 +23,9 @@ namespace Sungero.Examples.Module.Docflow.Server
       InitializationLogger.Debug("Init: Create custom mark kinds.");
       CreateMarkKind(Sungero.Examples.IncomingInvoices.Resources.PaymentMarkName, Sungero.Examples.Constants.Contracts.IncomingInvoice.PaymentMarkKindSid,
                      Sungero.Examples.Constants.Contracts.IncomingInvoice.PaymentMarkKindClass, Sungero.Examples.Constants.Contracts.IncomingInvoice.PaymentMarkKindMethod);
+
+      CreateMarkKind(Sungero.Examples.Memos.Resources.SignedMarkName, Sungero.Examples.Constants.Docflow.Memo.SignMarkKindSid,
+                     Sungero.Examples.Constants.Docflow.Memo.SignMarkKindClass, Sungero.Examples.Constants.Docflow.Memo.SignMarkKindMethod);
       
       CreateMarkKind(Sungero.Examples.ContractualDocuments.Resources.PaginalApproveMarkName, Sungero.Examples.Constants.Contracts.Contract.PaginalApproveMarkKindSid,
                      Sungero.Examples.Constants.Contracts.Contract.PaginalApproveMarkKindClass, Sungero.Examples.Constants.Contracts.Contract.PaginalApproveMarkKindMethod);
