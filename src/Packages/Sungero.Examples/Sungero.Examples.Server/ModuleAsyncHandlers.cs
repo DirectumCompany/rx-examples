8462f506658324b80e029709a5cca82934641b55
Котегов Валентин <kotegov_VS@directum.ru>
2025-11-24 07:33:01 +0400

    #457392 подъем версии решения до 25.3.0

--- src/Packages/Sungero.Examples/Sungero.Examples.Server/ModuleAsyncHandlers.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.Server/ModuleAsyncHandlers.cs
@@ -6,7 +6,7 @@ using Sungero.CoreEntities;
 
 namespace Sungero.Examples.Server
 {
-  public class ModuleAsyncHandlers
+  public partial class ModuleAsyncHandlers
   {
 
   }
---
+++ src/Packages/Sungero.Examples/Sungero.Examples.Server/Sungero.Docflow/Queries/CreateStoragePolicySettings_mssql.sql
@@ -0,0 +1,27 @@
+if exists (select * from information_schema.tables where table_name = '{0}')
+  drop table {0}
+
+select r.Storage, rdk.DocumentKind,
+  case when r.DocDateType = 'DocumentDate' then dateadd(DD, -1 * r.DaysToMove, '{1}') else '2100-01-01 00:00:00.000' end as DocumentDate,
+  case when r.DocDateType = 'Modified' then dateadd(DD, -1 * r.DaysToMove, '{1}') else '2100-01-01 00:00:00.000' end as Modified,
+  case when r.DocDateType = 'LastRead' then dateadd(DD, -1 * r.DaysToMove, '{1}') else '2100-01-01 00:00:00.000' end as LastRead,
+  case when r.Discriminator = '9fed5653-77e7-4543-b071-6586033907ef' then r.[Priority] else r.[Priority] + (select coalesce(max(r2.[Priority]), 0) from Sungero_Docflow_StoragePolicy r2 where r2.Discriminator = '9fed5653-77e7-4543-b071-6586033907ef') end as [Priority],
+  case when coalesce(r.NextRetention, '{1}') <= '{1}' then 1 else 0 end as NeedRetention
+into {0}
+  from Sungero_Docflow_StoragePolicy r
+  join Sungero_Docflow_SPolicyDocKind rdk
+    on rdk.StoragePolicy = r.Id
+	where r.Status = 'Active';
+
+insert into {0}
+select r.Storage, rdk.Id,
+  case when r.DocDateType = 'DocumentDate' then dateadd(DD, -1 * r.DaysToMove, '{1}') else '2100-01-01 00:00:00.000' end as DocumentDate,
+  case when r.DocDateType = 'Modified' then dateadd(DD, -1 * r.DaysToMove, '{1}') else '2100-01-01 00:00:00.000' end as Modified,
+  case when r.DocDateType = 'LastRead' then dateadd(DD, -1 * r.DaysToMove, '{1}') else '2100-01-01 00:00:00.000' end as LastRead,
+  case when r.Discriminator = '9fed5653-77e7-4543-b071-6586033907ef' then r.[Priority] else r.[Priority] + (select coalesce(max(r2.[Priority]), 0) from Sungero_Docflow_StoragePolicy r2 where r2.Discriminator = '9fed5653-77e7-4543-b071-6586033907ef') end as [Priority],
+  case when coalesce(r.NextRetention, '{1}') <= '{1}' then 1 else 0 end as NeedRetention
+  from Sungero_Docflow_StoragePolicy r
+  join Sungero_Docflow_DocumentKind rdk
+    on 1=1
+	where r.Status = 'Active'
+	  and not exists (select 1 from Sungero_Docflow_SPolicyDocKind k where k.StoragePolicy = r.Id);
