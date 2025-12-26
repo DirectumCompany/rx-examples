9783764d6815e58ef7a5912e0bc1604862a294e0
Babushkina_AV <babushkina_av@directum.ru>
2025-12-26 17:24:18 +0400

    #462047 перетряхнула функции

--- src/Packages/Sungero.Examples/Sungero.Examples.ClientBase/AcquaintanceTask/AcquaintanceTaskActions.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.ClientBase/AcquaintanceTask/AcquaintanceTaskActions.cs
@@ -11,28 +11,7 @@ namespace Sungero.Examples.Client
   {
     public virtual void RemindParticipants(Sungero.Domain.Client.ExecuteActionArgs e)
     {
-      var incompleteAssignments = Sungero.Workflow.Assignments.GetAll()
-        .Where(a => Equals(a.Task, _obj) && 
-               a.Status == Sungero.Workflow.AssignmentBase.Status.InProcess)
-        .ToList();
-      
-      Logger.Debug($"RemindParticipants. Found {incompleteAssignments.Count} incomplete assignments");
-      
-      var sentCount = 0;
-      foreach (var assignment in incompleteAssignments)
-      {
-        var performer = Sungero.Examples.Employees.As(assignment.Performer);
-        if (performer == null)
-          continue;
-        
-        var subject = assignment.Subject;
-        var text = $"У вас невыполненное задание: {assignment.Subject}";
-        Functions.AcquaintanceTask.Remote.SendNotificationToEmployee(_obj, performer, subject, text);
-        
-        sentCount++;
-      }
-      
-      Logger.Debug($"RemindParticipants. Sent {sentCount} notifications");
+      Sungero.Examples.Module.RecordManagement.PublicFunctions.Module.RemindAcquaintanceTaskParticipants(_obj);
     }
 
     public virtual bool CanRemindParticipants(Sungero.Domain.Client.CanExecuteActionArgs e)
@@ -41,5 +20,4 @@ namespace Sungero.Examples.Client
     }
 
   }
-
 }
--- src/Packages/Sungero.Examples/Sungero.Examples.Server/AcquaintanceTask/AcquaintanceTaskServerFunctions.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.Server/AcquaintanceTask/AcquaintanceTaskServerFunctions.cs
@@ -10,12 +10,41 @@ namespace Sungero.Examples.Server
   partial class AcquaintanceTaskFunctions
   {
     /// <summary>
+    /// Напомнить участникам о невыполненных заданиях.
+    /// </summary>
+    [Remote]
+    public virtual void RemindParticipants()
+    {
+      var incompleteAssignments = Sungero.Workflow.Assignments.GetAll()
+        .Where(a => Equals(a.Task, _obj) && 
+               a.Status == Sungero.Workflow.AssignmentBase.Status.InProcess)
+        .ToList();
+      
+      Logger.Debug($"RemindParticipants. Found {incompleteAssignments.Count} incomplete assignments");
+      
+      var sentCount = 0;
+      foreach (var assignment in incompleteAssignments)
+      {
+        var performer = Sungero.Examples.Employees.As(assignment.Performer);
+        if (performer == null)
+          continue;
+        
+        var subject = assignment.Subject;
+        var text = $"У вас невыполненное задание: {assignment.Subject}";
+        this.SendNotificationToEmployee(performer, subject, text);
+        
+        sentCount++;
+      }
+      
+      Logger.Debug($"RemindParticipants. Sent {sentCount} notifications");
+    }
+    
+    /// <summary>
     /// Отправить уведомление сотруднику.
     /// </summary>
     /// <param name="employee">Сотрудник.</param>
     /// <param name="subject">Тема уведомления.</param>
     /// <param name="text">Текст уведомления.</param>
-    [Public, Remote]
     public virtual void SendNotificationToEmployee(Sungero.Examples.IEmployee employee, string subject, string text)
     {
       if (employee == null)
@@ -24,7 +53,10 @@ namespace Sungero.Examples.Server
       var channel = employee.NotificationChannel;
       
       if (channel == null || channel == Sungero.Examples.Employee.NotificationChannel.DoNotNotify)
+      {
+        Logger.Debug($"SendNotificationToEmployee. Cannot send message to Employee={employee.Id} because of his notify channel settings");
         return;
+      }
       
       Logger.Debug($"SendNotificationToEmployee. Send message to Employee={employee.Id} via channel={channel.Value}");
       if (channel == Sungero.Examples.Employee.NotificationChannel.Email)
@@ -33,13 +65,19 @@ namespace Sungero.Examples.Server
         {
           Sungero.Notifications.PublicFunctions.Module.SendEmail(employee.Email, subject, text);
         }
+        else
+        {
+          Logger.Debug($"SendNotificationToEmployee. Cannot send message to Employee={employee.Id} because of an empty e-mail");
+        }
       }
       else if (channel == Sungero.Examples.Employee.NotificationChannel.SMS)
       {
+        Logger.Debug($"SendNotificationToEmployee. SMS sending not implemented");
         // TODO: Реализовать отправку SMS
       }
       else if (channel == Sungero.Examples.Employee.NotificationChannel.Omni)
       {
+        Logger.Debug($"SendNotificationToEmployee. Omni sending not implemented");
         // TODO: Реализовать отправку через Омни
       }
     }
