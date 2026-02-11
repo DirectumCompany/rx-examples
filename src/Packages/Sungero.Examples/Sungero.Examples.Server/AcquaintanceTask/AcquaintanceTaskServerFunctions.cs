c188a8591c15e05b19f7da860b617086c3fe4c7d
Naborshchikova_DS <Naborshchikova_DS@directum.ru>
2026-02-11 13:09:34 +0400

    #472784 Правка уведомления о рассылке

--- src/Packages/Sungero.Examples/Sungero.Examples.Server/AcquaintanceTask/AcquaintanceTaskServerFunctions.cs
+++ src/Packages/Sungero.Examples/Sungero.Examples.Server/AcquaintanceTask/AcquaintanceTaskServerFunctions.cs
@@ -175,7 +175,7 @@ namespace Sungero.Examples.Server
       var subject = Docflow.PublicFunctions.Module.TrimQuotes($"Результат отправки уведомлений по задаче от {taskDate}.");
       var activeText = Docflow.PublicFunctions.Module.
         TrimQuotes($"Уведомления о задании от {taskDate} {Hyperlinks.Get(acquaintanceTask)} отправлены.\n\n" +
-                   $"Список:\n- Отправлено: {postedMessageCount} из {entries.Count}\n" +
+                   $"Список:\n- Отправлено: {postedMessageCount}\n" +
                    $"- Ошибок: {errorMessageCount}\n- Отключены уведомления: {notNotificatedCount}\n");
       var task = SimpleTasks.CreateWithNotices(subject, acquaintanceTask.Author);
       task.ActiveText = activeText;
